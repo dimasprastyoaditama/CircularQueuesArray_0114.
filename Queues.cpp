028d75ef84ded2aa0022495364afcd50af3f2bde
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-12 16:00:07 +0700

    hapus metode tampilan yang tidak digunakan dari kelas Antrean

--- Queues.cpp
+++ Queues.cpp
@@ -72,29 +72,5 @@ public:
         }
     }
 
-    void display()
-    {
-        int FRONT_position = FRONT;
-        int REAR_position = REAR;
-
-        // cek apakah antrian kosong
-        if (FRONT == -1)
-        {
-            cout << "Queue is empty\n";
-            return;
-        }
-
-        cout << "\nElements in the queue are...\n";
-
-        //Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
-        if (FRONT_position <= REAR_position)
-        {
-            while (FRONT_position <= REAR_position)
-            {
-                cout << queue_array[FRONT_position] << " ";
-                FRONT_position++;
-            }
-            cout << endl;
-        }
-        
+    
 }
