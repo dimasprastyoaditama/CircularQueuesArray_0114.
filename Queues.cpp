5a97c95ae2090eefe8b21c8db03a04cdce1458f0
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-12 15:48:12 +0700

    mengecek apakah antrian penuh

--- Queues.cpp
+++ Queues.cpp
@@ -22,5 +22,12 @@ public:
         cin >> num;
         cout << endl;
 
+        //1. cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; //1.a
+            return;
+        }
+
         
 }
