1de9a43cc82b9db9ee77bac3ccddea96efc3eb6b
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-12 15:55:33 +0700

    memperbaiki indentasi dan menambahkan penanganan untuk posisi REAR dalam metode penyisipan

--- Queues.cpp
+++ Queues.cpp
@@ -36,5 +36,12 @@ public:
             REAR = 0; // 2.b
         }
         else
+        {
+            //Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
         
 }
